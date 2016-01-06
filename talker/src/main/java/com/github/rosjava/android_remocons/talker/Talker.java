b13a499df17f764af6e5c2f0fdfbc726e57c0763
sriharish01 <sriharishsrinivasan@gmail.com>
2016-01-07 00:35:42 +0530

    added Orientation and Camera

--- talker/src/main/java/com/github/rosjava/android_remocons/talker/Talker.java
+++ talker/src/main/java/com/github/rosjava/android_remocons/talker/Talker.java
@@ -22,7 +22,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-
+import android.view.Display;
 
 import java.io.IOException;
 import java.util.ArrayList;
